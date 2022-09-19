e3c92a7adaa9264affd356622a4c285e1a1cf6b1
swri <aripsuwari@gmail.com>
2022-09-19 20:48:34 +0700

    first commit

---
+++ Belajar/sesi-01/hello-world/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+	fmt.Printf("Muhammad Arif Suwari\n")
+
+	fmt.Print("Hello", " Arif\n")
+	fmt.Print("Hello", " ", "Arif\n")
+}
---
+++ Belajar/sesi-01/variables/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	var name string = "Suwari"
+	var age int = 22
+
+	var nameComplete = "Muhammad Arif Suwari"
+
+	println("ini adalah namanya", name)
+	println("ini adalah umurnya", age)
+
+	print("ini adalah nama lengkap ", nameComplete)
+}
